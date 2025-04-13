ef251d7852f3e52af3f46f6166180682f9341818
Firdauz Tri Anggoro <86070626+FirTA@users.noreply.github.com>
2025-04-13 15:14:56 +0700

    fixing target api for front end

--- frontend/src/api/post.js
+++ frontend/src/api/post.js
@@ -1,19 +1,22 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://apelmusic-project-api.vercel.app/api",
+  baseURL:
+    "https://apelmusic-api-eec4dkbcezejfhhz.indonesiacentral-01.azurewebsites.net/api",
 });
 
 export const APIRequest = axios.create({
   // baseURL: "http://localhost:8471/api",
   // baseURL: "http://52.237.194.35:2032/api",
-  baseURL: "https://apelmusic-project-api.vercel.app/api",
+  baseURL:
+    "https://apelmusic-api-eec4dkbcezejfhhz.indonesiacentral-01.azurewebsites.net/api",
 });
 
 export const APIRequestWithHeaders = axios.create({
   // baseURL: "http://localhost:8471/api",
   // baseURL: "http://52.237.194.35:2032/api",
-  baseURL: "https://apelmusic-project-api.vercel.app/api",
+  baseURL:
+    "https://apelmusic-api-eec4dkbcezejfhhz.indonesiacentral-01.azurewebsites.net/api",
   headers: {
     Authorization: localStorage.getItem("token")
       ? `Bearer ${localStorage.getItem("token")}`
--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -2,7 +2,8 @@ import Axios from "axios";
 
 export const APIRequest = Axios.create({
   // baseURL: "http://localhost:8471/api",
-  baseURL: "https://apelmusic-project-api.vercel.app/api",
+  baseURL:
+    "https://apelmusic-api-eec4dkbcezejfhhz.indonesiacentral-01.azurewebsites.net/api",
 });
 
 const validateToken = (token) => {
